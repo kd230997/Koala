dc2e8bcffd811483dc3294cc9f9ac3e8c8f7da67
NguyenKhacDuy <kd230997@gmail.com>
2024-10-09 16:25:21 +0700

    update

--- sql_queries/TABLE_champions.sql
+++ sql_queries/TABLE_champions.sql
@@ -1,15 +1,1 @@
-CREATE DATABASE TFT_POOL;
 
-USE TFT_POOL;
-
-CREATE TABLE champion (
-  `id` INT PRIMARY KEY,
-  `character_name` VARCHAR(255),
-  `data` JSON
-)
-
-CREATE TABLE item (
-  `id` INT PRIMARY KEY,
-  `item_name` VARCHAR(255),
-  `data` JSON
-)
